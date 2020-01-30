eba13bba621141cb3dc31e67f9bb8016d3da3ff0
sasha walker <sashawalker31@gmail.com>
2020-01-30 04:05:13 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  n = 0
+  while n < array.length
+  yield array[n]
+  n+=1   
+end
+return array
+end
+array = [1,2,3]
+my_each(array) do |x|
+  return x
 end
